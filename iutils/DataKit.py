ef4f3b101bee36eca80792b882126bbd8cf09316
Bravebirds <5015893067@qq.com>
2021-08-09 23:05:43 +0800

    :zap:fix：请求参数中无法插入null值进行提交

--- iutils/DataKit.py
+++ iutils/DataKit.py
@@ -69,6 +69,8 @@ def conversType(dict_map:dict,disable_data:list=[]) -> dict:
                 dict_map[key] = conversType(dict_map=dict_map[key],disable_data=disable_data)
             elif str(dict_map[key]).isdigit() and str(key) not in disable_data:
                 dict_map[key] = int(dict_map[key])
+            elif str(dict_map[key]) =="null": # 统一处理str无法转化None
+                dict_map[key] =None
         return json.dumps(dict_map,ensure_ascii=False).replace('\\"','"').replace('"{',"{").replace('}"',"}") # 临时打个补丁 后续若报错则需再次做兼容
     else:
         raise TypeError("传入的参数不是dict类型 %s" % (type(dict_map)))
@@ -83,19 +85,3 @@ def dataRegroup(old_data: dict, temp={}) ->dict:
     if isinstance(old_data,dict):
         for k,v in old_data.items():
             print(k,v)
-
-if __name__ == '__main__':
-    from iutils.Loader import Loader
-    from BaseSetting import Route
-    from iutils.Template import Template
-    general_data = Loader.jsonFile(Route.joinPath("test_json","create_virtual_product.json"))
-    print("待处理的公共数据：",general_data)
-    replace_str = {'category_id': 15888, 'name': '自动化_虚拟商品7eN95a',
-                   'pic': 'http://file.vetscloud.com/2008e57c7feb8135f2754ba939a1bb41,http://file.vetscloud.com/830beab89f997daf4ff62d9c50dc98c5,http://file.vetscloud.com/3e62fed5130d6a9f8dd196b4719f4a6e,http://file.vetscloud.com/9db24b0c2c4a9d2edce96cabadc1a521,http://file.vetscloud.com/d55dc27a512ede0547e99d9fdb894e18',
-                   'selling_point': '', 'term_value': 1627257600, 'market_price': 157, 'retail_price': 157,
-                   'center_pic': 'http://file.vetscloud.com/0c97a173a6c78848a8fc554f7a65eab6'}
-    print(type(replace_str),json.dumps(replace_str,ensure_ascii=False))
-    dict_data = Template(general_data).subStitute(mapping=replace_str, type="dict")
-    convers_data = conversType(dict_data)
-    print(dict_data,convers_data)
-    print(getTargetValue(general_data))
