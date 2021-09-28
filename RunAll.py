edceef55010ab17d2f09ac9caae2ed4aa2f9b196
YuYanQing5920 <501893067@qq.com>
2021-09-28 15:19:19 +0800

    :bug: fix:没有用例时运行RunAll.py会导致异常且没有原因说明

--- RunAll.py
+++ RunAll.py
@@ -27,20 +27,17 @@ if __name__ == '__main__':
     allure_result = Route.getPath("allure_result")
     allure_report = Route.getPath("allure_report")
     file = os.path.basename(sys.argv[0])
-    try:
-        logger.info("""
-                                 _    _         _      _____         _
-                      __ _ _ __ (_)  / \\  _   _| |_ __|_   _|__  ___| |_
-                     / _` | '_ \\| | / _ \\| | | | __/ _ \\| |/ _ \\/ __| __|
-                    | (_| | |_) | |/ ___ \\ |_| | || (_) | |  __/\\__ \\ |_
-                     \\__,_| .__/|_/_/   \\_\\__,_|\\__\\___/|_|\\___||___/\\__|
-                       |_|Starting Time %s
-                    """ % (Moment.getTime("%Y-%m-%d %H:%M:%S")))
-        # debug 模式下调用
-        pytest.main([Route.getPath("test_path"), "--alluredir", allure_result,'-W','ignore:Module already imported:pytest.PytestWarning'])
-        shell.invoke("allure generate {} -c -o {} --clean".format(allure_result, allure_report))
-        shell.invoke("allure open %s" % (Route.getPath("allure_report")))
-        # 生产模式调用
-        # shell.invoke("python3 -m pytest -vs --alluredir  {}".format(allure_report))
-    except Exception as e:
-        logger.error("初始化脚本批量执行失败！", e)
+    logger.info("""
+                             _    _         _      _____         _
+                  __ _ _ __ (_)  / \\  _   _| |_ __|_   _|__  ___| |_
+                 / _` | '_ \\| | / _ \\| | | | __/ _ \\| |/ _ \\/ __| __|
+                | (_| | |_) | |/ ___ \\ |_| | || (_) | |  __/\\__ \\ |_
+                 \\__,_| .__/|_/_/   \\_\\__,_|\\__\\___/|_|\\___||___/\\__|
+                   |_|Starting Time %s
+                """ % (Moment.getTime("%Y-%m-%d %H:%M:%S")))
+    # debug 模式下调用
+    pytest.main([Route.getPath("test_path"), "--alluredir", allure_result,'-W','ignore:Module already imported:pytest.PytestWarning'])
+    shell.invoke("allure generate {} -c -o {}".format(allure_result, allure_report))
+    shell.invoke("allure open %s" % (Route.getPath("allure_report")))
+    # 生产模式调用
+    # shell.invoke("python3 -m pytest -vs --alluredir  {}".format(allure_report))
--- conftest.py
+++ conftest.py
@@ -16,11 +16,15 @@ from iutils.YamlUtils import YamlHandle
 def pytest_terminal_summary(terminalreporter):
     '''收集测试结果'''
     total = terminalreporter._numcollected
-    passed= len([i for i in terminalreporter.stats.get('passed', []) if i.when != 'teardown'])
-    failed=len([i for i in terminalreporter.stats.get('failed', []) if i.when != 'teardown'])
-    error=len([i for i in terminalreporter.stats.get('error', []) if i.when != 'teardown'])
-    skipped=len([i for i in terminalreporter.stats.get('skipped', []) if i.when != 'teardown'])
-    successful = len(terminalreporter.stats.get('passed', []))/terminalreporter._numcollected*100
-    duration = time.time() - terminalreporter._sessionstarttime
-    data = {"total":total,"passed":passed,"failed":failed,"error":error,"skipped":skipped,"successful":successful,"duration":duration}
-    YamlHandle.writeOjb(os.path.join(Route.getPath("workspaces"),"summary.yaml"),data,"w")
+    if not bool(terminalreporter.stats):
+        raise "暂时没有有效用例或开启了代理！！"
+    else:
+        passed = len([i for i in terminalreporter.stats.get('passed', []) if i.when != 'teardown'])
+        failed = len([i for i in terminalreporter.stats.get('failed', []) if i.when != 'teardown'])
+        error = len([i for i in terminalreporter.stats.get('error', []) if i.when != 'teardown'])
+        skipped = len([i for i in terminalreporter.stats.get('skipped', []) if i.when != 'teardown'])
+        successful = len(terminalreporter.stats.get('passed', [])) / terminalreporter._numcollected * 100
+        duration = time.time() - terminalreporter._sessionstarttime
+        data = {"total": total, "passed": passed, "failed": failed, "error": error, "skipped": skipped,
+                "successful": successful, "duration": duration}
+        YamlHandle.writeOjb(os.path.join(Route.getPath("workspaces"), "summary.yaml"), data, "w")
